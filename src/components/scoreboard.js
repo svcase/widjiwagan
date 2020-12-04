14d042c909ada7ca5862b2f0f5098907087d7ef2
Sam Case <samcase@Sams-MacBook-Air-2.local>
2020-12-04 16:39:32 -0600

    change scoreboard text

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -5,7 +5,7 @@ function ScoreBoard(props) {
   return (
     <div className="scoreBoard">
       <h2 className="scoreTitle">Total Score:</h2>
-      <h5 className="scoreGuide">(final pickup = 100)</h5>
+      <h5 className="scoreGuide">(safe trip = 100)</h5>
       <h3 className="score">{props.score}</h3>
       <h2 className="scoreTitle">Fatigue level:</h2>
       <h5 className="scoreGuide">(max 10)</h5>
