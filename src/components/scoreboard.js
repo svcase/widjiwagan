b8a931f147e68829fb2ad528c10b5da33637c5b0
Sam Case <samcase@Sams-MacBook-Air-2.local>
2020-12-04 15:48:07 -0600

    Initial commit

---
+++ src/components/scoreboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ScoreBoard(props) {
+
+  return (
+    <div className="scoreBoard">
+      <h2 className="scoreTitle">Total Score:</h2>
+      <h5 className="scoreGuide">(final pickup = 100)</h5>
+      <h3 className="score">{props.score}</h3>
+      <h2 className="scoreTitle">Fatigue level:</h2>
+      <h5 className="scoreGuide">(max 10)</h5>
+      <h3 className="score scoreBottom">{props.tired}</h3>
+    </div>
+  )
+};
+
+export default ScoreBoard;
